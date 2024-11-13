d0c8df22152f71f46eca9cda799ba5666d48a0db
KAMESEMPAIONE <volodyacy@gmail.com>
2024-11-13 14:31:20 +0200

    added requests logger

---
+++ src/logger/logger.middleware.ts
@@ -0,0 +1,34 @@
+import { Injectable, NestMiddleware } from "@nestjs/common"
+import * as morgan from "morgan"
+import * as fs from "fs"
+import * as path from "path"
+import * as fsP from "fs/promises"
+import { NextFunction } from "express"
+import { format } from "date-fns"
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const date = `[${format(new Date(), "dd/MM/yyyy HH:mm:ss")}]`
+    const day = `${format(new Date(), "dd-MM-yyyy")}`
+    const dirPath = path.join(process.cwd(), "logs")
+
+    if (!fs.existsSync(dirPath)) {
+      fsP.mkdir(dirPath)
+    }
+
+    const logStream = fs.createWriteStream(path.join(dirPath, `${day}.log`), {
+      flags: "a",
+    })
+
+    morgan.token("custom-date", () => {
+      return `${date}`
+    })
+
+    morgan(`:remote-addr -- :custom-date :method  :url  :status  :user-agent`, {
+      stream: logStream,
+    })(req, res, () => {})
+
+    next()
+  }
+}
